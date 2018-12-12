68b7413917ae55a14c405b6e1a80c38f7eb9e85b
zakhap <zhs2104@columbia.edu>
2018-12-12 14:23:10 -0500

    Initial Commit

---
+++ AR-DEMO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR-DEMO
+//
+//  Created by _hap on 12/12/18.
+//  Copyright © 2018 Zak Hap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
